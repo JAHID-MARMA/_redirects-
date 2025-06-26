fc8ab2d0b580a26923b3528ff408d05b4efba68c
JAHID-MARMA <162045420+JAHID-MARMA@users.noreply.github.com>
2025-06-26 18:19:38 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Jahid's Intelligence (React + Replit)</h1>
+      <p>This is your ready-to-run React app in Replit.</p>
+    </div>
+  );
+}
+
+export default App;
